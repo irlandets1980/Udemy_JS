7f1be95a35026a919fb73f47534f1f76dab5fe31
irlandets1980 <irlandets1980@gmail.com>
2020-03-12 23:50:43 +0300

    slider

--- Section 4/task 13(send data in veb servis)/js/script.js
+++ Section 4/task 13(send data in veb servis)/js/script.js
@@ -111,8 +111,10 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let form = document.querySelector('.main-form'),
         input = form.getElementsByTagName('input'),
-        statusMessage = document.createElement('div');
+        statusMessage = document.createElement('div'),
+        contactForm = document.querySelector('.contact-form');
     statusMessage.classList.add('status');
+
 
     form.addEventListener('submit', function (event) {
         event.preventDefault();
@@ -120,14 +122,14 @@ window.addEventListener('DOMContentLoaded', function () {
 
         let request = new XMLHttpRequest();
         request.open('POST', 'server.php');
-       // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-       request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        // request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
 
         let formData = new FormData(form);
         ///превращаем формдату в JSON
         let obj = {};
-        formData.forEach(function(value, key){
-           obj[key] = value;
+        formData.forEach(function (value, key) {
+            obj[key] = value;
         });
         let json = JSON.stringify(obj);
         ///////////////////////
@@ -148,6 +150,51 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    /////////slider///////
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    showSlides(slideIndex);
 
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach((item) => item.style.display = 'none'); //современный перебор с коллбэком - стрелочной функцией
+        dots.forEach((item) => item.classList.remove('dot-active'));
 
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function () {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function () {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function (event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+    });
 });
