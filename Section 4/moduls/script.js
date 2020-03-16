b5b6e3942160ded87cd43f4cb7ddc2e551d83bc3
irlandets1980 <irlandets1980@gmail.com>
2020-03-16 10:22:12 +0300

    moduls

---
+++ Section 4/moduls/script.js
@@ -0,0 +1,37 @@
+///////1//////////////
+let number = 1;
+
+(function () {
+    let number = 2;
+    console.log(number);
+
+    return console.log(number + 3);
+}());
+
+console.log(number);
+
+//////////2/////////////
+
+let user =  (function(){
+    let privat = function(){
+        console.log('I am privat');
+    };
+    return {
+        sayHello : function(){
+            console.log('Hello!');
+        }
+    };
+}());
+
+//////3//////////////
+let user =  (function(){
+    let privat = function(){
+        console.log('I am privat');
+    };
+    let sayHello = function(){
+        console.log('Hello!');
+    };
+    return {
+        sayHello : sayHello
+    };
+}());
